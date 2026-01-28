ca6653c301222bc811882d0ed72efc26594b4411
Thomas Woroniak <thomasworoniak@gmail.com>
2026-01-28 13:02:34 -0600

    add CRUD, MongoDB/Express features

---
+++ models/Idea.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const ideaSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    summary: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    tags: {
+      type: [String],
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Idea = mongoose.model("Idea", ideaSchema);
+
+export default Idea;
--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import ideaRouter from "./routes/ideaRoutes.js";
+import { errorHandler } from "./middleware/errorHandler.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+// Connect to MongoDB
+connectDB();
+
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use("/api/ideas", ideaRouter);
+
+// 404 Fallback
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
